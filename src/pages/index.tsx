df4f88fb9b04e162cac88a2cdf83c315628afb7f
ycjcl868 <45808948@qq.com>
2019-08-10 15:44:53 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function() {
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
           <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
+            Getting Started Umi
           </a>
         </li>
       </ul>
